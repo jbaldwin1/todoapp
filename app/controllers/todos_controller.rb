c74a19802a89f8334bdcb021f06167979a8ad389
John <jbaldwin117@gmail.com>
2017-09-28 03:34:42 +0000

    Added index and links

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -33,7 +33,15 @@ class TodosController < ApplicationController
         else
             render 'edit'
         end
-    end 
+    end
+        
+        
+    def index
+        @todos = Todo.all
+    end
+        
+        
+    
     
     
     
